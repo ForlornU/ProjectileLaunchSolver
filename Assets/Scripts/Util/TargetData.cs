de44325e283a164cd934859a7a877b6d9964546c
ForlornU <101473036+ForlornU@users.noreply.github.com>
2023-08-22 15:13:02 +0200

    Removed Drag from TargetData

--- Assets/Scripts/Util/TargetData.cs
+++ Assets/Scripts/Util/TargetData.cs
@@ -4,12 +4,10 @@ public struct TargetData
 {
     public Transform targetObject;
     public Vector3 targetPosition;
-    public float dragCoefficient;
 
-    public TargetData(Vector3 mouseClickPosition, Transform target, float drag)
+    public TargetData(Vector3 mouseClickPosition, Transform target)
     {
         targetObject = target;
         targetPosition = mouseClickPosition;
-        dragCoefficient = drag;
     }
 }
